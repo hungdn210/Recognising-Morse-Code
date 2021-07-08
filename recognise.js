0a55b7cd67d0862e3f67f45e29d898f9ae6165b9
hungdn210 <hungdn210@gmail.com>
2021-07-09 06:52:56 +0700

    test7

--- recognise.js
+++ recognise.js
@@ -1,125 +1,160 @@
-function Board() {
- 
-  this.update = () => {
-    //clean the left screen and set the value again
-    if(need_clear) { 
-      ctx.fillStyle = "white";
-      ctx.fillRect(0, 0, width_1, height_1);
-      need_clear = 0;
-      for(let i = 0; i < horiSqCnt; i++) {
-        for(let j = 0; j < vertSqcnt; j++) {
-          grid[i][j].seen = 0;
+function Recognise() {
+  this.data = () => {
+    numberData = {
+      "0": [
+        [-3,  3,  3,  3,  3, -1],
+        [ 3,  3, -1, -3,  1,  3],
+        [ 3, -2, -3, -3, -3,  3],
+        [ 3, -3, -3, -3, -3,  3],
+        [ 3, -3, -3, -3, -3,  3],
+        [ 3, -3, -3, -3, -3,  3],
+        [ 3,  1, -3, -3,  1,  3],
+        [ 2,  3,  3,  3,  3,  3]
+      ],
+      "1": [
+        [-3,  1,  3,  3, -1, -3],
+        [ 2,  2,  3,  3, -1, -3],
+        [-3, -3,  1,  3, -1, -3],
+        [-3, -3,  1,  3, -1, -3],
+        [-3, -3,  1,  3, -1, -3],
+        [-3, -3,  1,  3, -3, -3],
+        [-3, -3,  1,  3, -2, -3],
+        [ 1,  2,  3,  3,  2,  0]
+      ],
+      "2": [
+        [ 2,  3,  3,  3,  3, -3],
+        [ 3,  0, -3, -3,  3,  3],
+        [ 0, -3, -3, -3, -2,  3],
+        [-3, -3, -3, -2,  1,  2],
+        [-3, -3, -3, -1,  3,  1],
+        [-3, -3,  0,  3,  2, -3],
+        [-3,  2,  3,  0, -3, -3],
+        [ 2,  3,  3,  3,  3,  3]
+      ],
+      "3": [
+        [ 3,  3,  3,  3,  3, -1],
+        [ 2, -2, -3, -3,  1,  3],
+        [-3, -3, -3, -3, -2,  3],
+        [-3, -3, -3, -2,  3,  3],
+        [-3, -3,  0,  3,  3,  3],
+        [-3, -3, -3, -3, -2,  3],
+        [-3, -3, -3, -3,  0,  3],
+        [ 0,  3,  3,  3,  3,  3]
+      ],
+      "4": [
+        [ 3, -3, -3,  3, -3, -3],
+        [ 3, -3, -3,  3, -3, -3],
+        [ 3, -3, -3,  3, -3, -3],
+        [ 3,  2,  0,  3,  0,  0],
+        [ 2,  3,  1,  3,  3,  3],
+        [-3, -3, -2,  3, -2, -2],
+        [-3, -3, -3,  3, -2, -3],
+        [-3, -3, -2,  3, -2, -3]
+      ],
+      "5": [
+        [ 3,  3,  3,  3,  3,  3],
+        [ 3, -2, -3, -3, -3, -3],
+        [ 3, -3, -3, -3, -3, -3],
+        [ 3,  3,  3,  3,  3,  0],
+        [ 3,  3, -3, -3,  2,  3],
+        [-3, -3, -3, -3, -3,  3],
+        [-3, -3, -3, -3,  3,  3],
+        [-3,  3,  3,  3,  3,  3]
+      ],
+      "6": [
+        [-3,  1,  3,  3,  2, -3],
+        [ 1,  3,  2, -3, -3, -3],
+        [ 3,  3, -3, -3, -3, -3],
+        [ 3, -2,  2,  0, -2, -3],
+        [ 3,  3,  3,  3,  3,  2],
+        [ 3,  0, -3, -3,  1,  3],
+        [ 1,  2, -3, -3, -2,  3],
+        [ 0,  1,  3,  3,  3,  3]
+      ],
+      "7": [
+        [ 3,  3,  3,  3,  3,  3],
+        [-3, -3, -2, -2,  0,  3],
+        [-3, -3, -3, -2,  3,  2],
+        [-3, -3, -3,  1,  3, -3],
+        [-3, -3, -3,  3,  0, -3],
+        [-3, -3, -2,  3, -3, -3],
+        [-3, -2,  3,  2, -3, -3],
+        [-3, -2,  3,  0, -3, -3]
+      ],
+      "8": [
+        [ 3,  3,  3,  3,  3,  1],
+        [ 3,  1, -3, -3,  0,  1],
+        [ 3,  0, -3, -3,  0,  1],
+        [ 3,  3,  2,  0,  3,  1],
+        [ 0,  3,  3,  3,  3,  3],
+        [ 3,  3, -3, -3,  0,  3],
+        [ 3,  1, -3, -3, -2,  3],
+        [ 0,  3,  3,  3,  3,  3]
+      ],
+      "9": [
+        [ 0,  3,  3,  3,  3,  2],
+        [ 3,  3, -2, -1, -1,  3],
+        [ 3,  2, -3, -3, -1,  3],
+        [ 0,  3,  2,  3,  3,  3],
+        [-2,  0,  3,  3,  3,  2],
+        [-3, -3, -3, -3, -1,  3],
+        [-3, -3, -3, -3, -1,  3],
+        [-3, -3, -3, -3, -1,  3]
+      ]
+    };
+  }
+  this.check = () => { 
+    var curScore = 0, maxScore = 0, tempX = 0, tempY = 0;
+    bestNumber = 0;
+    for(let i = 0; i <= 9; i++) {
+      scoreNumbers[i] = 0;
+    }
+    //calculate the score of each number
+    for(let num = 0; num <= 9; num++) {
+      curScore = 0;
+      for(let y = 0; y < vertSqcnt; y++) {
+        for(let x = 0; x < horiSqCnt; x++) {
+          var curData = numberData[num][y][x];
+          grid[x][y].seen ? curScore += curData : curScore -= curData;
         }
       }
-    }
-    //draw the middle screen and third screen
-    if(need_run) {
-      corNumber = -1;
-      this.updateBoard_2();
-      this.zoom();
-      this.drawBorder();
-      this.drawBoard_2();
-      recognise.check();
-      recognise.drawThirdBoard();
-      need_run = 0;
-    }
-  }
-  
-  this.zoom = () => {
-    //increase the size of picture covered
-    ctx1.drawImage(canvas, border.x.min, border.y.min,
-      border.x.max - border.x.min, border.y.max - border.y.min, 0, 0, width_2, height_2);
-  };
-
-  this.drawBorder = () => {
-    //draw red border on the first screen
-    ctx.lineWidth = 1;
-    ctx.beginPath();
-    ctx.moveTo(0, border.y.min);
-    ctx.lineTo(width_1, border.y.min);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(0, border.y.max);
-    ctx.lineTo(width_1, border.y.max);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(border.x.min, 0);
-    ctx.lineTo(border.x.min, height_1);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(border.x.max, 0);
-    ctx.lineTo(border.x.max, height_1);
-    ctx.stroke();
-  }
- 
-  this.drawBoard_2 = () => {
-    //check the squares seen
-    for(let i = 0; i < width_1; i++) {
-      for(let j = 0; j < height_1; j++) {
-        var pixel = ctx1.getImageData(i, j, 1, 1);
-        var data = pixel.data;
-        if(data[1] != 255) grid[Math.floor(i / size_grid)][Math.floor(j / size_grid)].seen =  1;
+      scoreNumbers[num] = curScore;
+      if(maxScore < curScore) {
+        maxScore = curScore;
+        bestNumber = num;
       }
     }
-    //draw background 
-    ctx1.fillStyle = "#ccc";
-    ctx1.fillRect(0, 0, width_2, height_2);
- 
-    //draw lines
-    ctx1.strokeStyle = "black";
-    for(let i = 0; i <= width_2; i += size_grid) 
-      ctx1.strokeRect(i, 0, i, height_2);
-    for(let j = 0; j <= height_2; j += size_grid) 
-      ctx1.strokeRect(0, j, width_2, j);
-
-    //draw circle in the squares seen on the right screen
-    for(let i = 0; i < horiSqCnt; i++) {
-      for(let j = 0; j < vertSqcnt; j++) {
-        if(grid[i][j].seen) {
-          ctx1.beginPath();
-          ctx1.fillStyle = "black";
-          var tempX = grid[i][j].x, tempY = grid[i][j].y;
-          ctx1.fillRect(tempX - size_grid/2, tempY - size_grid/2 , size_grid, size_grid);
-          ctx1.fillStyle = "green";
-          ctx1.arc(tempX, tempY, 10, 0, 2 * Math.PI);
-          ctx1.stroke();
-          ctx1.fill();
+  }  
+  this.drawThirdBoard = () => {
+    //clean the screen
+    ctx2.beginPath();
+    ctx2.fillStyle = "white";
+    ctx2.fillRect(0, 0, width_3, height_3);
+    //draw score of 9 numbers
+    ctx2.beginPath();
+    ctx2.font = "20px Tahoma";
+    for(let i = 0; i <= 9; i++) {
+      ctx2.strokeText("number " + i + ":", 10, height_3 / 11 * (i + 1)- 10);
+      ctx2.strokeText(scoreNumbers[i] + "%", width_3 / 1.25, height_3 / 11 * (i + 1)- 10);
+    }
+    //the most similar score
+    ctx2.strokeText("The best number", 10, height_3 - 10);
+    ctx2.strokeText(bestNumber, width_3 / 1.25, height_3 - 10);
+    ctx2.stroke();
+    scoreNumbers.sort((a, b) => a - b);          
+    console.log(scoreNumbers);
+  }
+  this.learn = () => { 
+    if(corNumber != -1) {
+      for(let y = 0; y < vertSqcnt; y++) {
+        for(let x = 0; x < horiSqCnt; x++) {
+          var curData = numberData[corNumber][y][x];
+          if(grid[x][y].seen) 
+            curData < maxPoint ? curData++ : 0;
+          else curData > minPoint ? curData-- : 0;
+          numberData[corNumber][y][x] = curData;
         }
       }
-    }
-  }
- 
-  this.updateBoard_2 = () => {
-    //set up Red border 
-    border.x.min = width_1;
-    border.y.min = height_1;
-    border.x.max = 0;
-    border.y.max = 0;
-    //find the location of border
-    for(let i = 1; i < width_1; i++) {
-      for(let j = 1; j < height_1; j++) {
-        var pixel = ctx.getImageData(i, j, 1, 1);
-        var data = pixel.data;
-        if(data[1] != 255) {
-          border.x.min == width_1 ? border.x.min = i : 0;
-          border.y.min > j ? border.y.min = j : 0;
-          border.x.max = i;
-          border.y.max = Math.max(border.y.max, j);
-        }
-      }
-    }
-
-    //fix problems with border 
-    if(border.x.max - border.x.min < (border.y.max - border.y.min) / 2) {
-      var temp = (border.x.max + border.x.min) / 2;
-      border.x.min = temp - (border.y.max - border.y.min) / 4;
-      border.x.max = temp + (border.y.max - border.y.min) / 4;
-    }
-
-    if(border.y.max - border.y.min < (border.x.max - border.x.min) / 2) {
-      var temp = (border.y.max + border.y.min) / 2;
-      border.y.min = temp - (border.x.max - border.x.min) / 4;
-      border.y.max = temp + (border.x.max - border.x.min) / 4;
     }
   }
 }
